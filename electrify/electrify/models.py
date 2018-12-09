46863bd7e3d0ace3e0afe516b0436603c0c2a349
Natchu Periathambi <natch@Natchus-MacBook-Pro.fritz.box>
2018-12-10 11:43:41 +1300

    Removing the model created for resulting information - class not required

--- electrify/electrify/models.py
+++ electrify/electrify/models.py
@@ -9,13 +9,3 @@ class RawData(models.Model):
     energy_flow_direction = models.IntegerField(choices=FLOW)
     icp_id = models.PositiveIntegerField()
 
-class AggregatedData(models.Model):
-    BUYER = "buyer"
-    SELLER = "seller"
-    ROLE = ((BUYER,"Buyer"),(SELLER,"Seller"))
-    icp_id = models.PositiveIntegerField()
-    matched_amount = models.DecimalField(max_digits=6,decimal_places=4)
-    read_date = models.DateField()
-    read_time = models.TimeField()
-    publish_datetime = models.DateTimeField()
-    buyer_seller = models.CharField(choices=ROLE,max_length=7)
